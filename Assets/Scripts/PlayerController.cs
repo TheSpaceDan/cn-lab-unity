b5d6078a24d4cd3e341dc135a601f02352c40707
thespacedan <daniyalrana2002@gmail.com>
2023-11-22 10:49:32 +0500

    level 1 is done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform viewPoint;
+    public float mouseSens = 1f;
+    private float verticalRotStore;
+    private Vector2 mouseInput;
+    private float horizontalInput, verticalInput;
+    public float speed = 5f;
+    Vector3 moveDirection;
+    private Rigidbody rb;
+
+    public bool invertLook;
+    private bool isGrounded;
+    public float jumpForce = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSens;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+
+        verticalRotStore += mouseInput.y;
+        verticalRotStore = Mathf.Clamp(verticalRotStore, -60f, 60f);
+
+        if (invertLook) {
+            viewPoint.rotation = Quaternion.Euler(verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
+        }
+        else
+        {
+            viewPoint.rotation = Quaternion.Euler(-verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed = 7f;
+        }
+        if (Input.GetButtonUp("Sprint"))
+        {
+            speed = 4f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = (transform.forward * verticalInput + transform.right * horizontalInput).normalized;
+        moveDirection.y = 0;
+
+        rb.MovePosition(rb.position + moveDirection * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.other.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.other.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
